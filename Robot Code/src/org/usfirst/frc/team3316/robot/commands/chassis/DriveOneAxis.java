17ef199a0debc77f06cf148c1312615826b14b7d
Jonathan Ohayon <me@jonathano.com>
2018-02-27 21:39:34 +0200

    Added lowPass to DriveOneAxis

--- Robot Code/src/org/usfirst/frc/team3316/robot/commands/chassis/DriveOneAxis.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/commands/chassis/DriveOneAxis.java
@@ -5,13 +5,14 @@ import org.usfirst.frc.team3316.robot.commands.DBugCommand;
 import org.usfirst.frc.team3316.robot.humanIO.Joysticks.AxisType;
 import org.usfirst.frc.team3316.robot.humanIO.Joysticks.JoystickType;
 import org.usfirst.frc.team3316.robot.utils.PIDControllers;
+import org.usfirst.frc.team3316.robot.utils.Utils;
 
 import edu.wpi.first.wpilibj.PIDController;
 
 public class DriveOneAxis extends DBugCommand {
 
 	private JoystickType type;
-	private double speedFactor;
+	private double speedFactor, lowestValue;
 	private PIDController pidYaw;
 
 	public DriveOneAxis(JoystickType type) {
@@ -21,6 +22,7 @@ public class DriveOneAxis extends DBugCommand {
 
 	@Override
 	protected void init() {
+		lowestValue = (double) config.get("chassis_LowPassFilter_LowestValue");
 		speedFactor = (double) config.get("chassis_SpeedFactor_Current");
 
 		pidYaw = PIDControllers.getYawPID((double) config.get("chassis_DriveDistance_PID_YAW_KP") / 1000,
@@ -34,9 +36,11 @@ public class DriveOneAxis extends DBugCommand {
 
 	@Override
 	protected void execute() {
+		// REMARK: The motors are set using the yaw PID.
 		double axisValue = Robot.joysticks.getAxis(AxisType.RightY, this.type);
-		Robot.chassis.currentLeftV = axisValue * speedFactor;
-		Robot.chassis.currentRightV = axisValue * speedFactor;
+		double lowPass = Utils.lowPassFilter(axisValue, lowestValue, 0.0);
+		Robot.chassis.currentLeftV = lowPass * speedFactor;
+		Robot.chassis.currentRightV = lowPass * speedFactor;
 	}
 
 	@Override
